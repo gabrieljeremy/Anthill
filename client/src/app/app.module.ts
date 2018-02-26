747d14d43407eddeb21bf582b0d8449f327637dc
nanosix <nanosix@gmail.com>
2018-02-26 10:18:17 +0100

    angular material add

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatIconModule, MatMenuModule} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    [BrowserAnimationsModule],
-    [MatButtonModule, MatCheckboxModule]
+    BrowserAnimationsModule,
+    MatButtonModule,
+     MatCheckboxModule,
+     MatIconModule,
+     MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-export class PizzaPartyAppModule { }
+
