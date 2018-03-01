b702bc4c8719f88276262aec4c312dbe7a902899
gabrieljeremy <gabriel.jeremy21@gmail.com>
2018-03-01 15:57:16 +0100

    login

--- client/src/app/user-invalid/login/login.component.ts
+++ client/src/app/user-invalid/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../shared/models/user.model';
+import { UserService } from '../../shared/services/user.service';
+
+import { FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(public userService: UserService) { }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl(),
+    });
+  }
+
+  onSubmit(user) {
+    const response = this.userService.login(user as User);
+    if (response['message']) {
+      console.log(response['message']);
+    } else {
+      sessionStorage.setItem('user',  JSON.stringify(response));
+    }
   }
 
 }
