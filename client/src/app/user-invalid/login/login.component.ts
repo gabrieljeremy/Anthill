7852c5a90085d7e889468ad7073b9c882888426e
gabrieljeremy <gabriel.jeremy21@gmail.com>
2018-03-05 11:39:35 +0100

    login

--- client/src/app/user-invalid/login/login.component.ts
+++ client/src/app/user-invalid/login/login.component.ts
@@ -5,6 +5,8 @@ import { UserService } from '../../shared/services/user.service';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { Router } from '@angular/router';
+
 declare var $: any;
 
 @Component({
@@ -14,9 +16,11 @@ declare var $: any;
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  constructor(public userService: UserService) { }
+  msg: String;
+  constructor(public userService: UserService, private router: Router) { }
 
   ngOnInit() {
+
     this.loginForm = new FormGroup({
       email: new FormControl(),
       password: new FormControl(),
@@ -26,16 +30,18 @@ export class LoginComponent implements OnInit {
       e.preventDefault();
       $('.dialogReset').toggleClass('hiddenForm');
     });
+
   }
 
   async onSubmit(user)  {
-    const response = await this.userService.login(user as User)
-
-      console.log(response);
+    const response = await this.userService.login(user as User);
+      // console.log(response);
       if (response['message']) {
+        this.msg = response['message'];
         console.log(response['message']);
       } else {
         sessionStorage.setItem('user',  JSON.stringify(response));
+        this.router.navigate(['/portal']);
       }
 
   }
